532fa34376db9de90ab7e517f400d70a7da40b5d
StarCpt <86216339+StarCpt@users.noreply.github.com>
2022-07-24 00:43:26 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Plugins;
+using HarmonyLib;
+using Sandbox.Game.Entities;
+using System.Reflection;
+
+namespace Disable_Export_Model
+{
+    public class Main : IPlugin
+    {
+        public void Init(object gameInstance)
+        {
+            new Harmony("DisableExportModel").PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+    [HarmonyPatch(typeof(MyCubeBuilder), "HandleExportInput")]
+    public class HandleExportInputPatch
+    {
+        public static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
